21d331da01159be1d3959663a66823ebffde8604
Shigeru-Kondou <concigel@yahoo.co.jp>
2012-11-04 23:33:18 +0900

    bug fix.

--- public/index.php
+++ public/index.php
@@ -191,8 +191,14 @@ $pdo = null;
 $(function() {
   $('#trades').tablesorter({ sortList: [[0,0]], widgets: ['zebra'] });
 });
+(function() {
+  var e = document.createElement('script'); e.async = true;
+  e.src = document.location.protocol + '//connect.facebook.net/ja_JP/all.js';
+  document.getElementById('fb-root').appendChild(e);
+}());
 window.fbAsyncInit = function() {
-  FB.init({appId: '<?php echo $facebook->getAppId(); ?>', status: true, cookie: true, xfbml: true});
+  FB.init({appId: '<?php echo $facebook->getAppId(); ?>', status: true, cookie: true});
+  //FB.init({appId: '<?php echo $facebook->getAppId(); ?>', status: true, cookie: true, xfbml: true});
   //FB.Canvas.setAutoGrow({ width: 810, height: 1280 });
   FB.Canvas.setAutoGrow();
 };
