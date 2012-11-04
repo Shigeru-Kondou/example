e0d3accd08dbba7fe8a47c3cb48f619f9f18b89c
Shigeru Kondou <concigel@yahoo.co.jp>
2012-11-04 18:36:34 +0900

    bug fix.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-$debug = true;
+$local = false;  // ローカルでテストするときは、Facebook API を使用しない。
+$debug = false;
 
 // Facebook API 使用準備
 // Facebook上のアプリ：キャンバスページのURL
@@ -27,10 +28,14 @@ if(isset($_POST['signed_request'])){
     $like_flag=0;
   }
 }else {
-  // URL直打ちの時など
-  echo "<script type='text/javascript'>top.location.href = '$APP_URL';</script>";
-  if ( $debug ) var_dump( $_POST );
-  $like_flag=2;
+  if ( $local ) {
+    $like_flag = 1;
+  }else {
+    // URL直打ちの時など
+    echo "<script type='text/javascript'>top.location.href = '$APP_URL';</script>";
+    if ( $debug ) var_dump( $_POST );
+    $like_flag=2;
+  }
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -38,10 +43,10 @@ if(isset($_POST['signed_request'])){
 <head>
 <title>取引価格検索</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/bootstrap.min.css">
 <link href="css/blue/style.css" rel="stylesheet">
-<link href="css/adjust.css"     rel="stylesheet">
-<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+<link rel="stylesheet" href="css/adjust.css">
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 <script type="text/javascript" src="js/jquery.tablesorter.min.js"></script>
 </head>
 <body>
@@ -50,15 +55,18 @@ if(isset($_POST['signed_request'])){
 <?php
 if ( $debug ) echo ("LIKE FLAG:".$like_flag."<br/>\n");
 
-if ( $like_flag==0 ){
+if ( $like_flag==0 && !$local ){
+  // いいねを促す画面を表示
   echo "<img src=\"images/sky-810-4.png\" width=\"810\">\n";
 } else {
 try {
-  require('database.php');
-  
+  if( $local ) {
+    require('database-local.php');
+  } else {
+    require('database.php');
+  }
   $pdo = new PDO("mysql:host=$server;dbname=$db", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
   $query = "SELECT * FROM ex_maizuru";
-  $areaname_query = "select areaname from ex_maizuru group by areaname";
   
   
 ?>
@@ -68,26 +76,50 @@ try {
 <div class="container">
 <h3>検索オプション</h3>
 <!-- form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post"><fieldset -->
-<form action="<?php echo ("https://" . $_SERVER["HTTP_HOST"]); ?>" method="post"><fieldset>
-<table class="table-striped">
+<form action="<?php echo ( ($local?"http://":"https://") . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]); ?>" method="post"><fieldset>
+<table class="table-striped"><thead></thead>
+<tbody>
 <?php
-  if($debug && isset($_POST["areaname"])) {
-    var_dump( $_POST );
-  }
-  $area_stmt = $pdo->prepare($areaname_query);
-  $area_stmt->execute();
-  $count=0;
+  //SELECT areaname,stationname FROM examples.ex_maizuru where (stationname like '東舞鶴') or (stationname like '松尾寺') group by areaname
+  $area_east = array("丸山中町","丸山口町","丸山町","丸山西町","亀岩町","京月町","倉梯町","南浜町","字余部上",
+  "字北吸","字千歳","字和田","字堂奥","字小倉","字小橋","字市場","字常","字平","字森","字河辺由里","字泉源寺","字浜","字田井",
+  "字行永","常新町","愛宕上町","愛宕下町","愛宕中町","愛宕浜町","桃山町","森本町","森町","清美が丘","白浜台","字長浜","字大波下",
+  "字溝尻","溝尻町","溝尻中町","矢之助町",
+  "行永東町","行永桜通り","八反田北町","八反田南町",
+  "字吉野","字朝来中","朝来西町","字松尾","字登尾","字鹿原",
+  "字安岡","安岡町","字田中","田中町","字吉坂","田園町","鹿原西町","字木ノ下",
+  "字野原","字瀬崎","字多祢寺"
+  );
+  
+  $area_west = array("天台新町","字七日市","字万願寺","字上安","字上安久","字下安久","字上福井","字下福井","字丹波","字京口","字京田","字今田","字伊佐津",
+  "字余部上","字余部下","字倉谷","字公文名","字円満寺","字北田辺","字南田辺","字喜多","字境谷","字天台","字女布","字寺内","字平野屋","字引土","字引土新",
+  "字朝代","字本","字福来","字竹屋","字紺屋","字西","字西吉原","字野村寺","字青井","字高野由里","字魚屋","昭和台","清美が丘","清道新町","白浜台",
+  "福来問屋町",
+  "字岡田由里","字東神崎","字西神崎","字中山","字桑飼上","字桑飼下","字丸田","字大川","字久田美","字西方寺","字寺田","字吉田","字水間",
+  "字真倉","字志高","字地頭","字別所","字布敷","字八田","字河原"
+  );
+  
   $wrap=9; 
-  while($row = $area_stmt->fetchObject()) {
-    if($count%$wrap==0) echo "<tr>";
+  echo ("<tr><td colspan='".$wrap."'>東舞鶴</td></tr>\n");
+  for($i=0; $i<count($area_east); $i++) {
+    if($i%$wrap==0) echo "<tr>";
     $checked="";
-    if( isset($_POST["areaname"]) && is_array($_POST["areaname"]) && in_array( $row->areaname, $_POST["areaname"] ) )  $checked="checked";
-    echo ("<td><label class=\"checkbox inline\"><input type=\"checkbox\" name=\"areaname[]\" value=\"".$row->areaname."\" ".$checked."/>".$row->areaname."</label></td>");
-    if($count%$wrap==($wrap-1)) echo "</tr>\n";
-    $count++;
+    if( isset($_POST["areaname"]) && is_array($_POST["areaname"]) && in_array( $area_east[$i], $_POST["areaname"] ) )  $checked="checked";
+    echo ("<td><label class=\"checkbox inline\"><input type=\"checkbox\" name=\"areaname[]\" value=\"".$area_east[$i]."\" ".$checked."/>".$area_east[$i]."</label></td>");
+    if($i%$wrap==($wrap-1)) echo "</tr>\n";
   }
-  if($count%$wrap!=0) echo "</tr>\n" ;
+  if(count($area_east)%$wrap!=0) echo "</tr>\n" ;
+  echo ("<tr><td colspan='".$wrap."'>西舞鶴</td></tr>\n");
+  for($i=0; $i<count($area_west); $i++) {
+    if($i%$wrap==0) echo "<tr>";
+    $checked="";
+    if( isset($_POST["areaname"]) && is_array($_POST["areaname"]) && in_array( $area_west[$i], $_POST["areaname"] ) )  $checked="checked";
+    echo ("<td><label class=\"checkbox inline\"><input type=\"checkbox\" name=\"areaname[]\" value=\"".$area_west[$i]."\" ".$checked."/>".$area_west[$i]."</label></td>");
+    if($i%$wrap==($wrap-1)) echo "</tr>\n";
+  }
+  if(count($area_west)%$wrap!=0) echo "</tr>\n" ;
 ?>
+</tbody>
 </table>
 <input type="hidden" name="signed_request" value="<?php echo (isset($_POST['signed_request'])?$_POST['signed_request']:''); ?>">
 <button type="submit" class="btn btn-mini">検索</button> <button type="reset" class="btn btn-mini">元に戻す</button>
@@ -99,7 +131,8 @@ try {
 <div class="container">
 <h3>凡例</h3>
 <p>都市計画は以下の略語を使用しております。</p>
-<table>
+<table class="table-striped"><thead></thead>
+<tbody>
 <tr><td>1低</td><td>第１種低層住居専用地域</td><td>2低</td><td>第２種低層住居専用地域</td></tr>
 <tr><td>1中</td><td>第１種中高層住居専用地域</td><td>2中</td><td>第２種中高層住居専用地域</td></tr>
 <tr><td>1住</td><td>第１種住居地域</td><td>2住</td><td>第２種住居地域</td></tr>
@@ -107,7 +140,8 @@ try {
 <tr><td>商業</td><td>商業地域</td><td>準工</td><td>準工業地域</td></tr>
 <tr><td>工業</td><td>工業地域</td><td>工専</td><td>工業専用地域</td></tr>
 <tr><td>調整</td><td>市街化調整区域</td><td>非線</td><td>市街化区域及び市街化調整区域外の都市計画区域</td></tr>
-<tr><td>準都</td><td>準都市計画区域</td><td>区外</td><td>都市計画区域外</td><td></td><td></td></tr>
+<tr><td>準都</td><td>準都市計画区域</td><td>区外</td><td>都市計画区域外</td></tr>
+</tbody>
 </table>
 <h3>本サービスについて</h3>
 <p>本サービスのデータについては、<a href="http://www.land.mlit.go.jp/webland/download.html" target="_brank">国土交通省不動産取引価格情報</a>ダウンロードサービスを使用しております。</p>
@@ -128,7 +162,6 @@ try {
       if ( $i!=count($_POST["areaname"])-1 ) $query .= " or ";
     }
   }
-  if( $debug ) echo ( $query . "<br/>\n" );
   $stmt= $pdo->prepare($query);
   $stmt->execute();
   
@@ -159,15 +192,17 @@ $pdo = null;
 
 <br>
 
+</div>
 <script type="text/javascript">
 $(function() {
-  $("#trades").tablesorter({ sortList: [[1,0]], widgets: ['zebra'] });
+  $('#trades').tablesorter({ sortList: [[0,0]], widgets: ['zebra'] });
 });
 window.fbAsyncInit = function() {
   FB.init({appId: '<?php echo $facebook->getAppId(); ?>', status: true, cookie: true, xfbml: true});
   FB.Canvas.setAutoGrow({ width: 810, height: 1280 });
 };
+</script>
 <?php
 } // end of else
 ?>
-</div></body></html>
+</body></html>
